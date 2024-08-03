3d6689aa236110ae03e3c24725d1fe875401f91a
icarus02. <maybeicarus@gmail.com>
2024-08-02 22:08:07 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,23 @@
-Config = {
-    Pay = true,
-    Price = 250,
+Config = Config or {}
+
+Config.Pay = true -- Dictates if an individual will pay upon repair
+
+Config.Price = 1000 -- Dictates price if Config.Pay is true
+
+Config.Location = {                   -- Polyzone Location for where Radial option is added
+    vec2(-190.88, -1322.99),
+    vec2(-197.52, -1322.89),
+    vec2(-197.21, -1331.01),
+    vec2(-190.88, -1331.01),
 }
+
+Config.MinZ = 25 -- How far down the Z Coord for the Polyzone is
+Config.MaxZ = 35 -- How far up the Z Coord for the Polyzone extends
+
+Config.PolyName = "BennysRepair" -- Name for Polyzone, ensure this isnt matching any other
+
+Config.SoundVol = 1.0
+
+
+
+
